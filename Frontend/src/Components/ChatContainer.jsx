887e0090640b927e38ee9a02ef589c95bc5a3b2a
nikhilsharma9548 <ns630403@gmail.com>
2025-09-30 21:15:57 +0530

    Push

--- Frontend/src/Components/ChatContainer.jsx
+++ Frontend/src/Components/ChatContainer.jsx
@@ -44,10 +44,10 @@ const ChatContainer = ({selectedUser, setSelectedUser, setOpenUser }) => {
               {msg.image ? (
                 <img src={msg.image} alt="" className='max-w-[450px] border border-slate-700 rounded-lg overflow-hidden mb-8'/>
               ) : (
-                <p className={`p-2 max-w-[600px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
+                <p className={`p-2 max-w-[600px] xs:text-lg font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
               )}
-              <div className='text-center text-xs'>
-                <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt="" className='w-7 rounded-full' />
+              <div className='text-center text-xs xs:text-sm'>
+                <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt="" className='w-7 xs:w-9 rounded-full' />
                 <p className='text-gray-500'>{formatMessageTime(msg.createdAt)}</p>
 
               </div>
@@ -58,17 +58,17 @@ const ChatContainer = ({selectedUser, setSelectedUser, setOpenUser }) => {
         </div>
         {/* --------------- bottom area --------------------- */}
 
-        <div className='absolute w-full bottom-0 right-0 flex items-center gap-3 p-3 '>
+        <div className='absolute w-full bottom-0 right-0 flex items-center gap-3 p-3 xs:p-4 '>
           <div className='flex-1  flex items-center bg-slate-100/12 px-3 rounded-full'>
             <input type="text" placeholder="send a message"
-             className='flex-1  text-sm p-3 border-none rounded-lg outline-none text-white placeholder-slate-500'/>
+             className='flex-1  text-sm xs:text-lg p-3 border-none rounded-lg outline-none text-white placeholder-slate-500'/>
             <input type="file" id='image' accept='image/png, image/jpeg' hidden/>
             <label htmlFor="image">
               <img src={assets.gallery_icon} alt="" className='w-5 mr-2  cursor-pointer'/>
             </label>
           </div>
           {/* <img src={assets.send_button} alt="" className='w-7 cursor-pointer' /> */}
-          <p className='text-2xl cursor-pointer mr-2'><IoIosSend/></p>
+          <p className='text-2xl xs:text-3xl cursor-pointer mr-2'><IoIosSend/></p>
         </div>
 
       </div>
@@ -77,7 +77,7 @@ const ChatContainer = ({selectedUser, setSelectedUser, setOpenUser }) => {
   ) : (
     <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-slate-800/10 max-md:hidden'>
       <img src={assets.logo} alt="" className='max-w-20' />
-      <p className='text-lg font-medium text-white'>Chat anytiime, anywhere</p>
+      <p className='text-lg  font-medium text-white'>Chat anytiime, anywhere</p>
     </div>
   )
 }
